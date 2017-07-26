c13cdc6430883c8ac37c262f373eb05332b835e8
Luca Weiss <luca@z3ntu.xyz>
2017-07-26 11:57:00 +0200

    Changes for Arch Linux as host

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 # Change your host GCC here.
-export HOST_MACHINE=x86_64-linux-gnu
+export HOST_MACHINE=x86_64-pc-linux-gnu
 
 # Change how many CPUs you want to use.
 export PROCS=1
 
 # Set this to your ccache path if using ccache.
-export CCACHE_DIRECTORY="/usr/lib/ccache"
+export CCACHE_DIRECTORY="/usr/lib/ccache/bin"
 
 # GNU Hurd target.
 export CPU=i586
