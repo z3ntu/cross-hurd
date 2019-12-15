b91055d701a8e2fd8ac562ccfc343fc260212556
Nathan Dehnel <5498361+Gooberpatrol66@users.noreply.github.com>
2019-12-15 02:45:11 -0600

    fix bootstrap.sh

--- config.sh
+++ config.sh
@@ -2,10 +2,10 @@
 export HOST_MACHINE=x86_64-pc-linux-gnu
 
 # Change how many CPUs you want to use.
-export PROCS=1
+export PROCS=4
 
 # Set this to your ccache path if using ccache.
 export CCACHE_DIRECTORY="/usr/lib/ccache"
 
 # GNU Hurd target.
-export CPU=i586
+export CPU=i686
