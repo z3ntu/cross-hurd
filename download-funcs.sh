8412b7149f4ae360009e7e0a341f6af3a8644462
Gitea <gitea@fake.local>
2020-10-10 23:29:48 -0500

    update glibc and mpfr

--- download-funcs.sh
+++ download-funcs.sh
@@ -74,13 +74,7 @@ download_glibc () {
    if [ -d glibc ]; then
       return 0
    fi
-   git clone https://git.savannah.gnu.org/git/hurd/glibc.git -b tschwinge/Roger_Whittaker &&
-   cd glibc &&
-   git clone https://git.savannah.gnu.org/git/hurd/libpthread.git &&
-   (for p in $SCRIPT_DIR/patches/glibc/*; do
-      apply_patch $p 1
-   done) &&
-   cd ..
+   git clone git://sourceware.org/git/glibc.git
 }
 
 unpack_gcc () {
