99ea8c6d6cf46648ddc347743774069128a161cf
Gitea <gitea@fake.local>
2020-10-11 13:45:29 -0500

    fix glibc download and remove patch

--- download-funcs.sh
+++ download-funcs.sh
@@ -74,7 +74,13 @@ download_glibc () {
    if [ -d glibc ]; then
       return 0
    fi
-   git clone git://sourceware.org/git/glibc.git
+   git clone https://git.savannah.gnu.org/git/hurd/glibc.git -b tschwinge/Roger_Whittaker &&
+   cd glibc &&
+   git clone https://git.savannah.gnu.org/git/hurd/libpthread.git &&
+   (for p in $SCRIPT_DIR/patches/glibc/*; do
+      apply_patch $p 1
+   done) &&
+   cd ..
 }
 
 unpack_gcc () {
