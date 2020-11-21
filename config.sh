4b545ad188ce4e16d32ea0cea8eb083c6c1e62d3
Luca Weiss <luca@z3ntu.xyz>
2020-11-21 16:12:04 +0100

    -j6

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 export HOST_MACHINE=x86_64-pc-linux-gnu
 
 # Change how many CPUs you want to use.
-export PROCS=1
+export PROCS=6
 
 # Set this to your ccache path if using ccache.
 export CCACHE_DIRECTORY="/usr/lib/ccache/bin"
